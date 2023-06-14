725460212e9547ab9f80802b28160e3d97c6afa6
AlvLay <alvlaykevin@gmail.com>
2023-06-14 09:45:26 +0800

    ai修改 2023/6/14 -2

--- js/ai.js
+++ js/ai.js
@@ -1,27 +1,70 @@
 ﻿var Ai = Ai || {};
 
-Ai.historyTable = {};		//历史表
+Ai.transpositionTable = {};
 
-//人工智能初始化
+// /**
+//  * 该函数用于初始化AI。
+//  * 它进行Alpha-Beta剪枝搜索，找到最佳走法及其值，并在界面上输出搜索结果。
+//  * 函数返回找到的最佳走法。
+//  *
+//  * @paramArray} pace - 当前游戏棋。
+//  * @returns {Array|boolean} - 找到的最佳走法，或false。
+//  */
+// Ai.init = function (pace) {
+//     Ai.move;
+//     Ai.treeDepth = 5;
+//     Ai.number = 0;
+//     Ai.setHistoryTable.lenght = 0;
+//     var initTime = new Date().getTime();
+//     var val = Ai.getAlphaBeta(-99999, 99999, Ai.treeDepth, rule.arr2Clone(player1.map), player1.my);
+//     if (!val || val.value == -8888) {
+//         Ai.treeDepth = 4;
+//         val = Ai.getAlphaBeta(-99999, 99999, Ai.treeDepth, rule.arr2Clone(player1.map), player1.my);
+//     }
+//     if (val && val.value != -8888) {
+//         var man = player1.mans[val.key];
+//         var nowTime = new Date().getTime();
+//         Ai.move = rule.createMove(rule.arr2Clone(player1.map), man.x, man.y, val.x, val.y);
+//         rule.get("moveInfo").innerHTML = '<h3>AI搜索结果：</h3>最佳着法：' +
+//             Ai.move +
+//             '<br />搜索深度：' + Ai.treeDepth + '<br />搜索分支：' +
+//             Ai.number + '个 <br />最佳着法评估：' +
+//             val.value + '分' +
+//             ' <br />搜索用时：' +
+//             (nowTime - initTime) + '毫秒';
+//         temp = rule.get("info").textContent;
+//         if(player1.my == -1) rule.get("info").innerHTML = temp + '\n' + '黑：' + Ai.move;
+//         else rule.get("info").innerHTML = temp + '\n' + '红：' + Ai.move;
+//         return [man.x, man.y, val.x, val.y];
+//     } else {
+//         return false;
+//     }
+// }
+
+
+/**
+ * 该函数用于初始化AI。
+ * 它进行迭代加深搜索，找到最佳走法及其值，并在界面上输出搜索结果。
+ * 函数返回找到的最佳走法。
+ *
+ * @param {Array} pace - 当前游戏棋谱。
+ * @returns {Array|boolean} - 找到的最佳走法，或false。
+ */
 Ai.init = function (pace) {
     Ai.move;
-    var initTime = new Date().getTime();
-    Ai.treeDepth = 4;
-
     Ai.number = 0;
-    Ai.setHistoryTable.lenght = 0;
-
-    var val = Ai.getAlphaBeta(-99999, 99999, Ai.treeDepth, rule.arr2Clone(player1.map), player1.my);
-    //var val = Ai.iterativeSearch(com.arr2Clone(player1.map),player1.my)
+    var initTime = new Date().getTime();
+    var val = Ai.iterativeSearch(rule.arr2Clone(player1.map), player1.my);
     if (!val || val.value == -8888) {
         Ai.treeDepth = 4;
-        val = Ai.getAlphaBeta(-99999, 99999, Ai.treeDepth, rule.arr2Clone(player1.map), player1.my);
+        val = Ai.iterativeSearch(rule.arr2Clone(player1.map), player1.my);
     }
-    //var val = Ai.iterativeSearch(com.arr2Clone(player1.map),player1.my);
     if (val && val.value != -8888) {
         var man = player1.mans[val.key];
         var nowTime = new Date().getTime();
-        Ai.move = rule.createMove(rule.arr2Clone(player1.map), man.x, man.y, val.x, val.y);
+        Ai.move = rule.createMove(rule.arr2Clone(player1.map), man.x, man.y, val.x, val.y);//一步棋
+
+        //下面都是在界面上输出
         rule.get("moveInfo").innerHTML = '<h3>AI搜索结果：</h3>最佳着法：' +
             Ai.move +
             '<br />搜索深度：' + Ai.treeDepth + '<br />搜索分支：' +
@@ -32,25 +75,67 @@ Ai.init = function (pace) {
         temp = rule.get("info").textContent;
         if(player1.my == -1) rule.get("info").innerHTML = temp + '\n' + '黑：' + Ai.move;
         else rule.get("info").innerHTML = temp + '\n' + '红：' + Ai.move;
+
+        // var data = {
+        //     "player": (player1.my === -1 ? "黑" : "红"),
+        //     "move": Ai.move
+        // };
+        //
+        // // 将数据追加到本地存储中
+        // var log = localStorage.getItem('log') || '';
+        // log += data.player + ': ' + data.move + '\n';
+        // localStorage.setItem('log', log);
+
+        // // 创建要存储的数据对象
+        // var data = {
+        //     "player": (player1.my === -1 ? "黑" : "红"),
+        //     "move": Ai.move
+        // };
+        //
+        // // 将数据转换为 JSON 字符串
+        // var jsonString = JSON.stringify(data);
+        //
+        // // 使用 FileSaver.js 库将 JSON 字符串写入本地文件
+        // var blob = new Blob([jsonString], {type: "application/json;charset=utf-8"});
+        // saveAs(blob, "js/lib.json");
+
+
         return [man.x, man.y, val.x, val.y];
     } else {
         return false;
     }
 }
 
-//迭代加深搜索着法
+
+/**
+ * 该函数用于进行迭代加深搜索。
+ * 它从初始深度开始，逐步增加深度，直到达到最大深度或超时时间。
+ * 函数返回找到的最佳走法及其值。
+ *
+ * @param {Array} map - 当前游戏棋盘状态。
+ * @param {number} my - 玩家方。
+ * @returns {Object} - 找到的最佳走法及其值。
+ */
 Ai.iterativeSearch = function (map, my) {
-    var timeOut = 100;
-    var initDepth = 1;
-    var maxDepth = 8;
-    Ai.treeDepth = 0;
-    var initTime = new Date().getTime();
-    var val = {};
+    var timeOut = 100;//超时时间
+    var initDepth = 1;//初始深度
+    var maxDepth = 8;//最大深度
+    Ai.treeDepth = 0;//当前搜索深度
+    var initTime = new Date().getTime();//获取时间
+    // var val = {};
     for (var i = initDepth; i <= maxDepth; i++) {
         var nowTime = new Date().getTime();
-        Ai.treeDepth = i;
-        Ai.aotuDepth = i;
-        var val = Ai.getAlphaBeta(-99999, 99999, Ai.treeDepth, map, my)
+        Ai.treeDepth = i;//控制搜索深度
+        //Ai.autoDepth = i;
+        var val = Ai.getAlphaBeta(-99999, 99999, Ai.treeDepth, map, my)//计算下一步最佳位置
+
+        // 将搜索结果存入置换表中
+        Ai.transpositionTable[JSON.stringify(map)] = {
+            depth: Ai.treeDepth,
+            value: val.value,
+            flag: 0, // 精确值
+        };
+
         if (nowTime - initTime > timeOut) {
             return val;
         }
@@ -58,12 +143,22 @@ Ai.iterativeSearch = function (map, my) {
     return false;
 }
 
-//取得棋盘上所有棋子
+
+/**
+ * 该函数用于获取当前玩家所有棋子的位置信息。
+ * 它遍历整个棋盘，找到所有属于当前玩家的棋子，并将它们的位置信息存储在一个数组中。
+ * 函数返回包含位置信息的man数组。
+ *
+ * @param {Array} map - 当前游戏棋盘状态。
+ * @param {number} my - 玩家方。
+ * @returns {Array} - 包含位置信息的man数组。
+ */
 Ai.getMapAllMan = function (map, my) {
     var mans = [];
     for (var i = 0; i < map.length; i++) {
         for (var n = 0; n < map[i].length; n++) {
             var key = map[i][n];
+            //该位置有棋子且属于player1
             if (key && player1.mans[key].my == my) {
                 player1.mans[key].x = n;
                 player1.mans[key].y = i;
@@ -71,24 +166,32 @@ Ai.getMapAllMan = function (map, my) {
             }
         }
     }
-    return mans;
+    return mans;//返回包含位置信息的man数组
 }
 
-//取得棋谱所有己方棋子的着法
+
+/**
+ * 该函数用于获取当前玩家所有可能的移动。
+ * 它首先获取当前玩家所有棋子的位置信息，然后计算每个棋子所有可能的移动。
+ * 函数返回包含所有可能移动的数组。
+ *
+ * @param {Array} map - 当前游戏棋盘状态。
+ * @param {number} my - 玩家方。
+ * @returns {Array} - 包含所有可能移动的数组。
+ */
 Ai.getMoves = function (map, my) {
-    var manArr = Ai.getMapAllMan(map, my);
-    var moves = [];
-    var foul = player1.isFoul;
+    var manArr = Ai.getMapAllMan(map, my);//获取棋子位置信息
+    var moves = [];//包含所有可能移动的数组
+    var foul = player1.isFoul;//如果存在长将情况，则会返回一个含有四个坐标值的数组
     for (var i = 0; i < manArr.length; i++) {
         var man = manArr[i];
-        var val = man.bl(map);
-
+        var val = man.bl(map);//计算并存储该位置所有可能的移动
         for (var n = 0; n < val.length; n++) {
             var x = man.x;
             var y = man.y;
             var newX = val[n][0];
             var newY = val[n][1];
-            //如果不是长将着法
+            //如果不是长将着法，则将该移动数值存入数组
             if (foul[0] != x || foul[1] != y || foul[2] != newX || foul[3] != newY) {
                 moves.push([x, y, newX, newY, man.key])
             }
@@ -96,33 +199,61 @@ Ai.getMoves = function (map, my) {
     }
     return moves;
 }
-//A:当前棋手value/B:对手value/depth：层级
+
+
+/**
+ * 该函数实现了Alpha-Beta剪枝算法，用于中国象棋游戏AI。
+ * 它以当前游戏棋盘状态、搜索树深度和玩家方为输入。
+ * 它生成玩家的所有可能走法，并使用带有Alpha-Beta剪枝的Negamax算法评估每个走法。
+ * 该函数返回找到的最佳走法及其值作为对象。
+ * 转置表用于存储先前评估的位置，以避免冗余计算。
+ * 该函数还实现了移动排序以提高搜索效率。
+ *
+ * @param {number} A - Alpha-Beta剪枝算法的Alpha值。
+ * @param {number} B - Alpha-Beta剪枝算法的Beta值。
+ * @param {number} depth - 搜索树的深度。
+ * @param {Array} map - 当前游戏棋盘状态。
+ * @param {number} my - 玩家方。
+ * @returns {Object} - 找到的最佳走法及其值。
+ */
 Ai.getAlphaBeta = function (A, B, depth, map, my) {
-    //var txtMap= map.join();
-    //var history=Ai.historyTable[txtMap];
-    //	if (history && history.depth >= Ai.treeDepth-depth+1){
-    //		return 	history.value*my;
-    //}
-    if (depth == 0) {
-        return { "value": Ai.evaluate(map, my) }; //局面评价函数;
+
+    var transpositionTable = Ai.transpositionTable;
+    var hash = JSON.stringify(map);
+
+    // 检查置换表
+    if (transpositionTable[hash] && transpositionTable[hash].depth >= depth) {
+        var ttEntry = transpositionTable[hash];
+        if (ttEntry.flag == 0) { // 精确值
+            return { value: ttEntry.value };
+        } else if (ttEntry.flag == 1 && ttEntry.value <= A) { // 下限值
+            return { value: ttEntry.value };
+        } else if (ttEntry.flag == 2 && ttEntry.value >= B) { // 上限值
+            return { value: ttEntry.value };
+        }
     }
-    var moves = Ai.getMoves(map, my); //生成全部走法;
-    //这里排序以后会增加效率
+
+
+    if (depth == 0) {
+        return { "value": Ai.evaluate(map, my) }; //返回当前棋局的评估值
+    }
+    //生成全部着法
+    var moves = Ai.getMoves(map, my);
+
 
     for (var i = 0; i < moves.length; i++) {
-
-
-        //走这个走法;
         var move = moves[i];
-        var key = move[4];
+        var key = move[4];//man.key
         var oldX = move[0];
         var oldY = move[1];
         var newX = move[2];
         var newY = move[3];
         var clearKey = map[newY][newX] || "";
 
-        map[newY][newX] = key;
-        delete map[oldY][oldX];
+        map[newY][newX] = key;//移动到新的为位置
+        delete map[oldY][oldX];//原位置上的信息被删除
+
+        //更新信息
         player1.mans[key].x = newX;
         player1.mans[key].y = newY;
 
@@ -135,10 +266,9 @@ Ai.getAlphaBeta = function (A, B, depth, map, my) {
                 map[newY][newX] = clearKey;
                 // player1.mans[ clearKey ].isShow = false;
             }
-
             return { "key": key, "x": newX, "y": newY, "value": 8888 };
-            //return rootKey;
         } else {
+            //采用负极大值算法，进行递归调用
             var val = -Ai.getAlphaBeta(-B, -A, depth - 1, map, -my).value;
             //val = val || val.value;
 
@@ -151,14 +281,17 @@ Ai.getAlphaBeta = function (A, B, depth, map, my) {
                 map[newY][newX] = clearKey;
                 //player1.mans[ clearKey ].isShow = true;
             }
+            //如果值大于beta，则进行beta剪枝
             if (val >= B) {
                 //将这个走法记录到历史表中;
                 //Ai.setHistoryTable(txtMap,Ai.treeDepth-depth+1,B,my);
                 return { "key": key, "x": newX, "y": newY, "value": B };
             }
+            //大于alpha，则更新alpha值
             if (val > A) {
                 A = val; //设置最佳走法;
-                if (Ai.treeDepth == depth) var rootKey = { "key": key, "x": newX, "y": newY, "value": A };
+                if (Ai.treeDepth == depth)
+                    var rootKey = { "key": key, "x": newX, "y": newY, "value": A };
             }
         }
     }
@@ -176,13 +309,16 @@ Ai.getAlphaBeta = function (A, B, depth, map, my) {
     return { "key": key, "x": newX, "y": newY, "value": A };
 }
 
-//奖着法记录到历史表
-Ai.setHistoryTable = function (txtMap, depth, value, my) {
-    Ai.setHistoryTable.lenght++;
-    Ai.historyTable[txtMap] = { depth: depth, value: value }
-}
 
-//评估棋局 取得棋盘双方棋子价值差
+/**
+ * 该函数用于评估当前棋局的价值。
+ * 它遍历整个棋盘，计算每个棋子的价值，并将它们相加。
+ * 函数返回当前棋局的总价值，乘以玩家方的数。
+ *
+ * @param {Array} map - 当前游戏棋盘状态。
+ * @param {number} my - 玩家方。
+ * @returns {number} - 当前棋局的总价值。
+ */
 Ai.evaluate = function (map, my) {
     var val = 0;
     for (var i = 0; i < map.length; i++) {
@@ -194,6 +330,6 @@ Ai.evaluate = function (map, my) {
         }
     }
     //val+=Math.floor(Math.random()*10);  //让AI走棋增加随机元素
-    Ai.number++;
+    Ai.number++;//记录搜索的节点数
     return val * my;
 }
--- js/bill.js
+++ js/bill.js
@@ -1,7 +1,12 @@
 ﻿var bill = bill || {};
 
-//初始化
+/**
+ * 初始化bill对象。
+ * 本函数用于检查是否存在棋谱。如果存在棋谱，则停止计时器，并将棋谱写入棋谱列表。
+ * 如果不存在棋谱，则每隔一秒调用自身进行检查。
+ */
 bill.init = function (){
+    //如果有棋谱，则调用
     if (rule.store){
         console.log("棋谱go");
         clearInterval(bill.timer);
@@ -11,13 +16,22 @@ bill.init = function (){
         rule.show();
     }else {
         console.log("棋谱out");
-        bill.timer = setInterval("bill.init()",1000);
+        bill.timer = setInterval("bill.init()",1000);//每隔一秒调用自身
     }
 }
 
-//把所有棋谱写入棋谱列表
+/**
+ * 该函数为棋谱列表添加选项
+ * 并为每个选项添加事件监听器
+ * 当选项改变时，在界面上显示对应的棋谱。
+ *
+ * @param {Array} map - 当前游戏棋盘状态。
+ * @returns null
+ */
 bill.setBillList = function (map){
     var list=rule.get("billList");
+    console.log(list)
+    //为每个棋谱创建一个option元素，并加入棋谱列表
     for (var i=0; i < rule.store.length; i++){
         var option = document.createElement('option');
         option.text='棋谱'+(i+1);
@@ -31,7 +45,17 @@ bill.setBillList = function (map){
     bill.setBox (rule.store[0], map)
 }
 
-//棋谱分析 写入
+
+/**
+ * 该函数根据棋谱中的移动信息
+ * 更新棋盘上的棋子位置
+ * 并在界面上显示移动过程。
+ *
+ * @param {Array} bl - 棋谱中的移动信息数组
+ * @param {number} inx - 当前移动的步数
+ * @param {Array} map - 当前游戏棋
+ * @returns {Array} - 更新后的棋盘
+ */
 bill.setMove = function (bl,inx,map){
     var map = rule.arr2Clone(map);
     for (var i=0; i<map.length; i++){
@@ -45,11 +69,12 @@ bill.setMove = function (bl,inx,map){
         }
     }
 
+    //解析字符串，使棋谱棋盘棋子移动
     for (var i=0; i<= inx ; i++){
         var n = i*4;
+        var x = bl[n+0];
         var y = bl[n+1];
         var newX = bl[n+2];
-        var x = bl[n+0];
         var newY = bl[n+3];
         if (rule.mans[map[newY][newX]]) {
             rule.mans[map[newY][newX]].isShow = false;
@@ -58,7 +83,7 @@ bill.setMove = function (bl,inx,map){
         rule.mans[map[y][x]].x = newX;
         rule.mans[map[y][x]].y = newY;
 
-        if (i == inx) {
+        if (i === inx) {
             rule.showPane(x,y,newX,newY);
         }
         map[newY][newX] = map[y][x];
@@ -83,13 +108,29 @@ bill.setBox = function (bl,initMap){
         h +='</li>\n\r';
     }
     rule.get("billBox").innerHTML = h;
+    let doms=rule.get("billBox").getElementsByTagName("li");
+    let index=[],step=-1;
+    for (let i=0;i<doms.length; i++) index[i]=doms[i].getAttribute("id").split("_")[1]
+    let nextB=document.getElementById("nextBn")
+    let lastB=document.getElementById("lastBn")
+    nextB.addEventListener("click",function (){
+        bill.setMove(bl,index[++step],initMap)
+        rule.show();
+    })
+    lastB.addEventListener("click",function (){
+        if(!(step<=-1)) {
+            bill.setMove(bl, index[--step], initMap)
+            rule.show();
+        }else{
+            console.log("已经是第一步")
+        }
 
-    var doms=rule.get("billBox").getElementsByTagName("li");
-
-    for (var i=0; i<doms.length; i++){
+    })
+    console.log(doms)
+    for (let i=0; i<doms.length; i++){
         doms[i].addEventListener("click", function(e) {
-            var inx = this.getAttribute("id").split("_")[1];
-            bill.setMove (bl , inx , initMap)
+            step=index[i];
+            bill.setMove (bl , index[step] , initMap)
             rule.show();
         })
     }
